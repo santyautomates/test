fefc2d0b3a4669a166e14f3e74f5c91f202a2369
santyautomates <darkprinceminion@gmail.com>
2026-03-03 13:17:08 +0000

    Remove Google provider and storage bucket resource
    Removed Google provider configuration and storage bucket resource.

--- main.tf
+++ main.tf
@@ -4,35 +4,4 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 5.0"
     }
-  }
-}
-
-provider "google" {
-  project = "aimlexplore"
-  region  = "us-central1"
-}
-
-resource "google_storage_bucket" "secure_bucket" {
-  name                        = "my-secure-bucket-001-unique"
-  location                    = "US"
-  uniform_bucket_level_access = true
-  public_access_prevention    = "enforced"
-  force_destroy               = false
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle_rule {
-    action {
-      type = "Delete"
-    }
-    condition {
-      age = 365
-    }
-  }
-
-  encryption {
-    default_kms_key_name = null
-  }
-}
+ 
